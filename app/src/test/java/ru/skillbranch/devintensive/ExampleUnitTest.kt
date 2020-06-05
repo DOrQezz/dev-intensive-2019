2aad7403eb42afedf90214833629188086e31655
DOrQezz <63741672+DOrQezz@users.noreply.github.com>
2020-06-05 19:04:48 +0300

    hometask_2 complete

---
+++ app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
@@ -0,0 +1,21 @@
+package ru.skillbranch.devintensive
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import ru.skillbranch.devintensive.extensions.TimeUnits
+import ru.skillbranch.devintensive.extensions.add
+import ru.skillbranch.devintensive.models.*
+import java.util.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
