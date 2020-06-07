781e5f26cd21f0bd1a3a9b7bf7a77ff181a88a85
Igor Fedorov <63741672+DOrQezz@users.noreply.github.com>
2020-06-07 23:25:35 +0300

    fixes2

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,31 +1,32 @@
 package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
-import android.content.Context.INPUT_METHOD_SERVICE
 import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 
-fun Activity.isKeyboardOpen(): Boolean {
-    val r = Rect()
-    val rootView = window.decorView
-    rootView.getWindowVisibleDisplayFrame(r)
-    val screenHeight = rootView.height
-    var heightDiff = screenHeight - (r.bottom - r.top)
-    val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
-    if (resourceId > 0) {
-        heightDiff -= resources.getDimensionPixelSize(resourceId)
-    }
-    if (heightDiff > 100) {
-        return true
-    }
-    return false
-}
-
-fun Activity.isKeyboardClosed() = !isKeyboardOpen()
 
 fun Activity.hideKeyboard() {
-    if (isKeyboardClosed()) return
-    val view = currentFocus ?: return
-    val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+    this.hideKeyboard(if (currentFocus == null) View(this) else currentFocus)
+}
+
+fun Activity.isKeyboardOpen(): Boolean{
+    val rootView = findViewById<View>(android.R.id.content)
+    val rect = Rect()
+    rootView.getWindowVisibleDisplayFrame(rect)
+    val heightDiff = rootView.height - rect.height()
+    val err = this.dpToPx(20F)
+
+    return heightDiff > err
+}
+
+private fun Activity.dpToPx(dp: Float): Float {
+    return TypedValue.applyDimension (TypedValue.COMPLEX_UNIT_DIP , dp, this.resources.displayMetrics)
+}
+private fun Activity.hideKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
+
+fun Activity.isKeyboardClosed(): Boolean = !this.isKeyboardOpen()
